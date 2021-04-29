ad6c0fe6c8f927edfd70ac3fc59c45583ec4ebab
anpigon <markan82@gmail.com>
2021-04-29 12:07:21 +0900

    200. Local State vs App-wide State

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
 
 import '../widgets/product_grid.dart';
+// import '../providers/products.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showFavoritesOnly = false;
+
   @override
   Widget build(BuildContext context) {
+    // final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShap'),
-      ),
-      body: ProductGrid(),
+      appBar: AppBar(title: const Text('MyShap'), actions: [
+        PopupMenuButton(
+          onSelected: (FilterOptions selectedValue) {
+            setState(() {
+              if (selectedValue == FilterOptions.Favorites) {
+                // productsContainer.showFavoritesOnly();
+                _showFavoritesOnly = true;
+              } else {
+                // productsContainer.showAll();
+                _showFavoritesOnly = false;
+              }
+            });
+          },
+          icon: Icon(Icons.more_vert),
+          itemBuilder: (_) => [
+            PopupMenuItem(
+              child: Text('Only Favorites'),
+              value: FilterOptions.Favorites,
+            ),
+            PopupMenuItem(
+              child: Text('Show All'),
+              value: FilterOptions.All,
+            ),
+          ],
+        ),
+      ]),
+      body: ProductGrid(_showFavoritesOnly),
     );
   }
 }
