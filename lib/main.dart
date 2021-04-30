3eb5e2831c5fbac10606a4632a9d694223a6b695
anpigon <markan82@gmail.com>
2021-04-30 11:34:22 +0900

    208. Providing an Orders Object

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail.screen.dart';
 import './screens/cart_screen.dart';
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (ctx) => Products()),
         ChangeNotifierProvider(create: (ctx) => Cart()),
+        ChangeNotifierProvider(create: (ctx) => Orders()),
       ],
       child: MaterialApp(
         title: 'MyShop',
