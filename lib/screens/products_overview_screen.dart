6ff118cae2f82867b1b8028a8dcbb3093c7527fd
anpigon <markan82@gmail.com>
2021-04-30 09:49:24 +0900

    203. Connecting the Cart Provider

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
-import '../widgets/product_grid.dart';
 // import '../providers/products.dart';
+import '../widgets/product_grid.dart';
+import '../providers/cart.dart';
+import '../widgets/badge.dart';
 
 enum FilterOptions {
   Favorites,
@@ -20,33 +22,49 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   Widget build(BuildContext context) {
     // final productsContainer = Provider.of<Products>(context, listen: false);
+    // final cart = Provider.of<Cart>(context);
     return Scaffold(
-      appBar: AppBar(title: const Text('MyShap'), actions: [
-        PopupMenuButton(
-          onSelected: (FilterOptions selectedValue) {
-            setState(() {
-              if (selectedValue == FilterOptions.Favorites) {
-                // productsContainer.showFavoritesOnly();
-                _showFavoritesOnly = true;
-              } else {
-                // productsContainer.showAll();
-                _showFavoritesOnly = false;
-              }
-            });
-          },
-          icon: Icon(Icons.more_vert),
-          itemBuilder: (_) => [
-            PopupMenuItem(
-              child: Text('Only Favorites'),
-              value: FilterOptions.Favorites,
+      appBar: AppBar(
+        title: const Text('MyShap'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  // productsContainer.showFavoritesOnly();
+                  _showFavoritesOnly = true;
+                } else {
+                  // productsContainer.showAll();
+                  _showFavoritesOnly = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, child) {
+              return Badge(
+                child: child,
+                value: cart.itemCount.toString(),
+              );
+            },
+            child: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {},
             ),
-            PopupMenuItem(
-              child: Text('Show All'),
-              value: FilterOptions.All,
-            ),
-          ],
-        ),
-      ]),
+          ),
+        ],
+      ),
       body: ProductGrid(_showFavoritesOnly),
     );
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/cart.dart';
 import '../screens/product_detail.screen.dart';
 import '../providers/product.dart';
 
@@ -18,6 +19,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -53,7 +55,13 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
             color: Theme.of(context).accentColor,
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(
+                product.id,
+                product.price,
+                product.title,
+              );
+            },
           ),
         ),
       ),
