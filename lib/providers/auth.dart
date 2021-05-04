f3267d775bc629465239bc2e71aa767554e6d2bf
anpigon <markan82@gmail.com>
2021-05-04 22:30:44 +0900

    276. Automatically Logging Users Out (After Some Time)

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 import '../models/http_exception.dart';
 
@@ -9,6 +10,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -59,6 +61,7 @@ class Auth with ChangeNotifier {
           ),
         ),
       );
+      _authLogout();
       notifyListeners();
       // return json.decode(response.body);
     } catch (error) {
@@ -78,6 +81,18 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _authLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
